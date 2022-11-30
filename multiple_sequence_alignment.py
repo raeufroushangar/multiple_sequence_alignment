66287acf8090548b0f9f2103f7f11d54bd214f0a
raeufroushangar <roushnroushan19@gmail.com>
2022-11-30 02:41:32 +0000

    Added comments

--- multiple_sequence_alignment.py
+++ multiple_sequence_alignment.py
@@ -4,7 +4,7 @@ from Bio import SeqIO
 import os
 
 #email used to access NCBI-GEO
-en.email = "bioinfo@gmail.com"
+en.email = "bioinfo@bioinfo.com"
 
 def ncbiFastaFilesDownloaderFunc (geneDic):
       """
@@ -34,21 +34,19 @@ def fileMergerFunc (dirList, outputFile):
                 data = f1.readlines()
             with open(outputFile, 'a') as f2:
                 f2.writelines(data)
-                f2.write("\n")
 
 
 if __name__ == '__main__':
 
     # Gene symbol and GenBank ID
     dict={'G3QWX4':'SRLZ01002358.1',
-        'E7F9E5':'CU652893.6',
-        'F7AH40':'QNVO02003165.1',
-        'Q5EGZ1':'CH474014.2',
-        'Q8R0I0':'AL671706.8',
-        'F6WXR7':'AAFR03034356.1',
-        'G1KTF3':'AAWZ02007819.1',
-        'Q56H28':'JAFEKA010000012.1',
-        'Q5RFN1':'NDHI03003400.1'}
+          'E7F9E5':'CU652893.6',
+          'F7AH40':'QNVO02003165.1',
+          'Q8R0I0':'AL671706.8',
+          'F6WXR7':'AAFR03034356.1',
+          'G1KTF3':'AAWZ02007819.1',
+          'Q56H28':'JAFEKA010000012.1',
+          'Q5RFN1':'NDHI03003400.1'}
     ncbiFastaFilesDownloaderFunc(dict)
-    fileMergerFunc(os.listdir(),"merged_fasta_files.fasta")
+    fileMergerFunc(os.listdir(),"muscle_input.fasta")
 

a8c7d93f03a10f49540968b211598e5af37b5ac9
raeufroushangar <roushnroushan19@gmail.com>
2022-11-30 03:02:37 +0000

    Submit job script

--- muscle.sh
+++ muscle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # -clw is for output to be in CLUSTALW format (FASTA)
-muscle -clw -in muscle_input.fasta -out muscle_output_clw.fasta
+muscle -clw -in muscle_input.fasta -out muscle_output.clw
 
